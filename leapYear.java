7a8dfbae8110c30a58932b806b3f0950bbae5cfb
yexingql <1012693008@qq.com>
2026-09-11 12:44:49 +0800

    first change

--- leapYear.java
+++ leapYear.java
@@ -11,8 +11,7 @@ public class leapYear{
                 //if year is divisible by 4, it is a leap year
                
                 if((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)))
-                        System.out.println("Year " + year + " is a leap year");
-                else
+                       
                         System.out.println("Year " + year + " is not a leap year");
         }
 }
